59b3830d71a0f62dfb04004eb43e2fce89544e29
Asmodeus5503 <124157290+Asmodeus5503@users.noreply.github.com>
2024-02-15 21:43:05 +0000

    Added User model to models.py. made database migarations

--- SustainAGarden/challenges/models.py
+++ SustainAGarden/challenges/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=200, unique=True)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    coins = models.IntegerField(default=0)
+    completed_challenges = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
+
