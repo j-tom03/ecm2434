e37c8af3cf5369b6c8fa9a5a47abbb0174e32b6f
Asmodeus5503 <124157290+Asmodeus5503@users.noreply.github.com>
2024-02-18 17:21:36 +0100

    Added challenge model to model.py, made DB migrations

--- SustainAGarden/challenges/models.py
+++ SustainAGarden/challenges/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 
 
 class User(models.Model):
@@ -12,4 +12,15 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class challenge(models.Model):
+    challenge_ID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    coins = models.IntegerField()
+    challenge_setter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
