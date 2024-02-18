1f48fc8e0b2c9f1c77b21a8fe905b7b02fbe16db
Asmodeus5503 <124157290+Asmodeus5503@users.noreply.github.com>
2024-02-18 17:25:58 +0100

    Made DB Migrations for previous commit

--- SustainAGarden/challenges/models.py
+++ SustainAGarden/challenges/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=200, primary_key=True)
     email = models.EmailField(max_length=200)
+    # these should be stored hashed not raw
     password = models.CharField(max_length=200)
     coins = models.IntegerField(default=0)
     completed_challenges = models.IntegerField(default=0)
