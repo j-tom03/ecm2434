faceb59ff0c41daf5b9e0e9fcd2505faf3abc5e3
Tom Cavilla <46577288+TMC330@users.noreply.github.com>
2024-02-20 13:40:00 +0000

    Javascript functions for creating a populating the plot grid.

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,17 @@
+function create_grid(width, height) {
+    const plot = document.getElementById('plot-container');
+    for (let i = 0; i < width * height; i++) {
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        plot.appendChild(tile)
+    }
+}
+
+function populate_t(tile_string) {
+    const plot = document.getElementById('plot-container');
+    const tiles = plot.childNodes;
+    for (let i = 0; i < tile_string.length; i++) {
+        tiles[i].textContent = tile_string[i];
+    }
+}
 
