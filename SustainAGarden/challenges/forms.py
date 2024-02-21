644736b2ce0ff728f38c7f241c8f6bb21f1ac279
Asmodeus5503 <124157290+Asmodeus5503@users.noreply.github.com>
2024-02-21 21:32:06 +0100

    Added user registration form Added some stubs to challenges\views.py copied the frontend stuff to the required folders to test the initial views

---
+++ SustainAGarden/challenges/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class userForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=200)
+    email = forms.EmailField(label='Email', max_length=200)
+    password = forms.CharField(label='Password', max_length=200, widget=forms.PasswordInput)
+
