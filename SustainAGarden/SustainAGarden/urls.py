ad117aa42915e8012d50e7bd95d3ff750337608a
jtom03 <james.thomas@hatless.studio>
2024-02-21 18:22:17 +0000

    modified urls and views to match the new app

--- SustainAGarden/SustainAGarden/urls.py
+++ SustainAGarden/SustainAGarden/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("challenges/", include("challenges.urls")),
     path('admin/', admin.site.urls),
+    path('factmatch/', include('factmatch.urls')),
 ]
