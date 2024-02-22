63365eeec2663a793b7d735c1d94af08c58c5c46
jtom03 <james.thomas@hatless.studio>
2024-02-22 11:26:18 +0000

    forms added for factmatch fact submission

--- SustainAGarden/factmatch/views.py
+++ SustainAGarden/factmatch/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Fact
+from .forms import NewFactForm
+
 # Create your views here.
 def give_fact(request):
     return HttpResponse("This is a fact")
+
+def add_fact(request):
+    if request.method == "POST":
+        form = NewFactForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("thanks for the fact!")
+
+    else:
+        form = NewFactForm()
+
+    return render(request, "name.html", {"form": form})
