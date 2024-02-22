016566abeb2c50f1d74b7d3a45561122ce809a26
jtom03 <james.thomas@hatless.studio>
2024-02-22 16:39:00 +0000

    resolved issues with setchallengeform

--- SustainAGarden/factmatch/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Fact
-
-class NewFactForm(forms.ModelForm):
-    fact = forms.CharField(label='Fact', max_length=200)
-    class Meta:
-        model = Fact
-        fields = ['fact']   
--- SustainAGarden/factmatch/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Fact(models.Model):
-    fact = models.CharField(max_length=200)
-    def __str__(self):
-        return self.fact
-    
