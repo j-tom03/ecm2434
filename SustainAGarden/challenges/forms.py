df6061cb7464a3604c9799efde75401a63a35a1f
Asmodeus5503 <124157290+Asmodeus5503@users.noreply.github.com>
2024-02-26 17:36:04 +0000

    Added form handling to index view, added some forms, added field to models

--- SustainAGarden/challenges/forms.py
+++ SustainAGarden/challenges/forms.py
@@ -5,6 +5,8 @@ class UserForm(forms.Form):
     username = forms.CharField(label='Username', max_length=200)
     email = forms.EmailField(label='Email', max_length=200)
     profile_image = forms.ImageField(label='Profile Image')
+    setter = forms.BooleanField(label='Setter', required=False)
+    institution = forms.BooleanField(label='Institution', required=False)
     password = forms.CharField(label='Password', max_length=200, widget=forms.PasswordInput)
 
 
