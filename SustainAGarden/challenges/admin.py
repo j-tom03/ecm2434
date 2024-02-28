0b6c1d6f5f0e56193436b104cfce85ecaff8a5d3
Lewis Goddard <156791469+redxap@users.noreply.github.com>
2024-02-28 23:11:42 +0000

    Add files via upload

--- SustainAGarden/challenges/admin.py
+++ SustainAGarden/challenges/admin.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.admin import UserAdmin
-
-from .forms import UserForm
-from .models import User
+from django.contrib import admin
 
 # Register your models here.
+
+
 from django.contrib import admin
 
 
@@ -23,29 +22,3 @@ class FlatPageAdmin(admin.ModelAdmin):
             },
         ),
     ]
-
-
-class CustomUserAdmin(UserAdmin):
-    add_form = UserForm
-    model = User
-    list_display = ["username", "email", "setter", "institution"]
-    list_filter = ["setter", "institution"]
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("email",)}),
-        ("Permissions", {"fields": ("setter", "institution")}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("username", "email", "password1", "password2", "setter", "institution"),
-            },
-        ),
-    )
-    search_fields = ["username"]
-    ordering = ["username"]
-
-
-admin.site.register(User, CustomUserAdmin)
